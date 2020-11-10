f8ef86108152d46c2a3a95e5eb5d540aa5eef592
sonhai32 <lamsonhai1379@gmail.com>
2020-11-10 19:52:29 +0700

    edit header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,129 +1,123 @@
-import React from 'react'
+import React from "react";
 
-import {Typography, Row, Col, Drawer} from 'antd'
-import { SearchOutlined, ShoppingCartOutlined, UserOutlined} from '@ant-design/icons'
-import './index.css'
+import { Typography, Row, Col, Drawer, Dropdown, Menu } from "antd";
+import {
+  SearchOutlined,
+  ShoppingCartOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import "./index.css";
 
-const styles = {
-    headerContainer: {
-      height: 80,
-      top: 0,
-      left: 0, 
-      position: 'fixed',
-      width: '100%',
-      background: '#fff',
-      boxShadow: '0px 1px 4px rgba(0,21,41,.08)'
-    },
-    headerLogoContainer:{
-display: 'flex', alignItems: 'center', paddingLeft: 5,
-    justifyContent: 'center',
-   },
-   navItem: {
-    height: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    padding: '2px 5px',
-    position: 'relative',
-    cursor: 'pointer',
-    fontWeight: 400,
-    margin: '0 5px',
-    color: '#0e172c'
-   },
-   iconContainer: {
-       display: 'flex',
-       alignItems: 'center',
-       justifyContent: 'center'
-   },
-   icon: {
-       margin: '0 5px',
-       fontSize: '26px',
-       cursor: 'pointer',
-       color: '#0e172c'
-   }
-}
-const {Title} = Typography;
+const { Title } = Typography;
+
+const menu = (
+  <Menu style={{ marginTop: 40 }}>
+    <Menu.Item key="0">
+      <a href="http://www.alipay.com/">1st menu item</a>
+    </Menu.Item>
+    <Menu.Item key="1">
+      <a href="http://www.taobao.com/">2nd menu item</a>
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="3">3rd menu item</Menu.Item>
+  </Menu>
+);
 const NavMenu = () => (
-<React.Fragment>
-
-            <div className='nav-item nav-before-line' style={styles.navItem}>
-                <span>
-                    Thoi Trang
-                </span>
-            </div>
-            <div className='nav-item nav-before-line' style={styles.navItem}>
-                <span>
-                    Thoi Trang
-                </span>
-            </div>
-            <div className='nav-item nav-before-line' style={styles.navItem}>
-                <span>
-                    Thoi Trang
-                </span>
-            </div>
-
-</React.Fragment>
-
+  <React.Fragment>
+    <div className="header-nav-item nav-before-line nav-before-circle">
+      <Dropdown overlay={menu} placement='bottomCenter'>
+        <span>Mỹ phẩm</span>
+      </Dropdown>
+    </div>
+    <div className="header-nav-item nav-before-line nav-before-circle">
+      <span>Mỹ phẩm</span>
+    </div>
+    <div className="header-nav-item nav-before-line nav-before-circle">
+      <span>Hàng tiêu dùng</span>
+    </div>
+    <div className="header-nav-item nav-before-line nav-before-circle">
+      <span>Hàng gia dụng</span>
+    </div>
+    <div className="header-nav-item nav-before-line nav-before-circle">
+      <span>Chăm sóc sức khỏe</span>
+    </div>
+  </React.Fragment>
 );
 
+const Header = () => {
+  const [userDrawer, setUserDrawer] = React.useState(false);
+  const [searchDrawer, setSearchDrawer] = React.useState(false);
+  const [cartDrawer, setCartDrawer] = React.useState(false);
 
-const Header = () =>{
-
-   const [userDrawer, setUserDrawer] = React.useState(false) 
-   const [searchDrawer, setSearchDrawer] = React.useState(false) 
-   const [cartDrawer, setCartDrawer] = React.useState(false) 
-
-const IconController = () =>(
+  const IconController = () => (
     <React.Fragment>
-        <SearchOutlined style={styles.icon} onClick={() => setSearchDrawer(true)} />
-        <UserOutlined style={styles.icon} onClick={() => setUserDrawer(true)} />
-        <ShoppingCartOutlined style={styles.icon} onClick={() => setCartDrawer(true)} />
+      <SearchOutlined
+        className="header-icon"
+        onClick={() => setSearchDrawer(true)}
+      />
+      <UserOutlined
+        className="header-icon"
+        onClick={() => setUserDrawer(true)}
+      />
+      <ShoppingCartOutlined
+        className="header-icon"
+        onClick={() => setCartDrawer(true)}
+      />
     </React.Fragment>
-)
+  );
 
-const UserDrawer = () =>(
-    <Drawer key='userDrawer'visible={userDrawer} placement='right' onClose={() => setUserDrawer(false)}>
-        <h1>
-            User
-        </h1>
+  const UserDrawer = () => (
+    <Drawer
+      key="userDrawer"
+      visible={userDrawer}
+      placement="right"
+      onClose={() => setUserDrawer(false)}
+    >
+      <h1>User</h1>
     </Drawer>
-)
-const SearchDrawer = () =>(
-    <Drawer key='searchDrawer'visible={searchDrawer} placement='right' onClose={() => setSearchDrawer(false)}>
-        <h1>
-            Search
-        </h1>
+  );
+  const SearchDrawer = () => (
+    <Drawer
+      key="searchDrawer"
+      visible={searchDrawer}
+      placement="right"
+      onClose={() => setSearchDrawer(false)}
+    >
+      <h1>Search</h1>
     </Drawer>
-)
-const CartDrawer = () =>(
-    <Drawer key='cartDrawer'visible={cartDrawer} placement='right' onClose={() => setCartDrawer(false)}>
-        <h1>
-            Cart
-        </h1>
+  );
+  const CartDrawer = () => (
+    <Drawer
+      key="cartDrawer"
+      visible={cartDrawer}
+      placement="right"
+      onClose={() => setCartDrawer(false)}
+    >
+      <h1>Cart</h1>
     </Drawer>
-)
-    return(
+  );
+  return (
     <React.Fragment>
-
-  <div style={styles.headerContainer} >
-    <Row style={{height: '100%'}}>
-      <Col flex md={6} sm={24} className='header-logo-container' style={styles.headerLogoContainer}>
-        <Title level={1} type='success'>
-            Logo
-        </Title>
-      </Col>
-      <Col md={12} style={styles.headerLogoContainer}>
-         <NavMenu /> 
-      </Col>
-<Col md={6} style={styles.iconContainer}>
-    <IconController />
-      </Col>
-    </Row>
-  </div>
-  <UserDrawer />
-  <CartDrawer />
-  <SearchDrawer />
-
+      <div className="header-container">
+        <Row style={{ height: "100%" }}>
+          <Col flex md={3} sm={24} xs={24} className="header-logo-container">
+            <Title level={1} type="success">
+              Logo
+            </Title>
+          </Col>
+          <Col md={17} sm={20} xs={24} className="header-logo-container">
+            <NavMenu />
+          </Col>
+          <Col md={4} sm={4} xs={24} className="header-logo-container">
+            <IconController />
+          </Col>
+        </Row>
+      </div>
+      <UserDrawer />
+      <CartDrawer />
+      <SearchDrawer />
     </React.Fragment>
-)}
+  );
+};
 
-export default Header
+export default Header;
